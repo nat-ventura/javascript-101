e6a8da90a2681b98af6a291db2ae00298f842e87
Natalie Villasana <navillasa@gmail.com>
2017-07-12 13:46:23 -0400

    added tip calculator exercises

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-function gree(whome, hwat) {
-    if (!whome) {
-        whome = "little dogs";
-    }
-    return "oh hi. " + hwat + ' to you, ' + whome;
-}
+// function gree(whome, hwat) {
+//     if (!whome) {
+//         whome = "little dogs";
+//     }
+//     return "oh hi. " + hwat + ' to you, ' + whome;
+// }
 
 // null is null. NOT NULL is TRUE. (kind of crazy..)..
 // recommend using pythontutor to see what is and what is NOT UNDEFINED
 
-console.log(gree(null, "warmest welcome"));
+// console.log(gree(null, "warmest welcome"));
 
 // var name = prompt("what's your big ol name ugly?")
 
@@ -25,5 +25,57 @@ console.log(gree(null, "warmest welcome"));
 //     printThing(name);
 // }
 
-
 // if you don't use var, a global variable is declared
+
+// HELLO YOU PART TWO
+
+// function hello(name) {
+//     if (name == undefined) {
+//         name = "world";
+//     }
+//     console.log("Hello ", name + "!");
+// }
+
+// hello("dogs")
+
+// MADLIB
+
+// function madlib(name, subject) {
+//     var newString = name + '\'s favorite subject in school is ' + subject + '.';
+//     return newString
+// }
+
+// console.log(madlib('Natalie','dogs'));
+
+// TIP CALCULATOR TWO
+
+// function tipAmount(bill, service) {
+//     var total = 0
+//     if (service == 'good') {
+//         total = 0.2 * bill + bill;
+//     } else if (service == 'fair') {
+//         total = 0.15 * bill + bill;
+//     } else if (service == 'poor') {
+//         total = 0.1 * bill + bill;
+//     }
+//     return total
+// }
+
+// console.log(tipAmount(100, 'good'))
+
+// TIP CALCULATOR THREE
+
+function splitAmount(bill, service, guests) {
+    var total = 0;
+    if (service == 'good') {
+        total = 0.2 * bill + bill;
+    } else if (service == 'fair') {
+        total = 0.15 * bill + bill;
+    } else if (service == 'poor') {
+        total = 0.1 * bill + bill;
+    }
+    var guestBill = total / guests;
+    return guestBill;
+}
+
+console.log(splitAmount(40, 'fair', 2));
