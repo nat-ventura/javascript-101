eba72f3d5e0209a28ffb733af42e09d724c1515a
Natalie Villasana <navillasa@gmail.com>
2017-07-12 11:35:35 -0400

    added a little if statement action

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,11 @@
-var name = "little dog";
+var name = prompt("what's your big ol name ugly?")
 
-var greeting = ("Hello " + name + "!");
+if (name.length >= 3) {
+    console.log("Hell0o u with the short ass name");
+} else {
+    var greeting = ("Hello " + name + "!");
+    console.log(greeting);
+    console.log("The length is", greeting.length + ".");
+}
 
 // if you don't use var, a global variable is declared
-
-console.log(greeting);
-console.log("The length is", greeting.length + ".");
