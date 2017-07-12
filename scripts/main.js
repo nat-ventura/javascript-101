541258b60b61eff20c292087987b80947cfd713e
Natalie Villasana <navillasa@gmail.com>
2017-07-12 11:42:15 -0400

    made the second if branch into a function

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
 var name = prompt("what's your big ol name ugly?")
 
-if (name.length >= 3) {
-    console.log("Hell0o u with the short ass name");
-} else {
+function printThing(name) {
     var greeting = ("Hello " + name + "!");
     console.log(greeting);
     console.log("The length is", greeting.length + ".");
+    // printing is not a return result
+}
+
+if (name.length <= 3) {
+    console.log("Hell0o u with the short ass name");
+} else {
+    printThing(name);
 }
 
 // if you don't use var, a global variable is declared
