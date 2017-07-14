9c29b68e8cca69740f9a2d9f80192d6a2b6a2662
Natalie Villasana <navillasa@gmail.com>
2017-07-14 11:22:13 -0400

    javascript phone book exercises

---
+++ scripts/function.js
@@ -0,0 +1,23 @@
+// var phonebookDict = {
+//     Alice: '703-493-1834',
+//     Bob: '857-384-1234',
+//     Elizabeth: '484-584-2923'
+// }
+
+// function lizPhone(phonebookDict) {
+//     console.log(phonebookDict['Elizabeth']);
+// }
+
+// function addContact(phonebookDict, name, num) {
+//     phonebookDict[name] = num;
+//     console.log(phonebookDict);
+// }
+
+// function deleter(dict, name) {
+//     phonebookDict[name] = "";
+//     console.log(dict);
+// }
+
+// lizPhone(phonebookDict);
+// addContact(phonebookDict, 'Kareem', '938-489-1234');
+// deleter(phonebookDict, 'Alice');
