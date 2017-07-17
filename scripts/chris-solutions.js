288bb3c92f8feacd4574a1b493b471d104f2d5af
Natalie Villasana <navillasa@gmail.com>
2017-07-17 11:30:21 -0400

    added city temperature dictionaries exercise for filter

--- scripts/chris-solutions.js
+++ scripts/chris-solutions.js
@@ -9,3 +9,33 @@ function makeFilter(fn) {
 
 var negative = makeFilter(isNegative);
 makeFilter(isNegative)([-1, -2, 3, 4]);
+
+// just step through it-- play around and pull in "actual values"
+// this way you can see what javascript is "doing"
+// orrrr try python tutor
+// LEARN TO THINK LIKE THE COMPILER
+
+var cities = [
+  { name: 'Los Angeles', temperature: 60.0},
+  { name: 'Atlanta', temperature: 52.0 },
+  { name: 'Detroit', temperature: 48.0 },
+  { name: 'New York', temperature: 80.0 }
+];
+
+function coolCities(cityArray) {
+    return cityArray.filter(function (city) {
+        return isCool(city.temperature);
+    });
+}
+
+function isCool(temp) {
+    return temp < 70;
+
+    function isCoolerThan(threshold) {
+        return function(temp) {
+            return temp < threshold;
+        }
+    }
+}
+
+var lessThan70 = isCoolerThan(70);
