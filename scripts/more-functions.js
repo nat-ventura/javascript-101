a40f990d44d3f335a0eb83d454eff4cbef6f5b9e
Natalie Villasana <navillasa@gmail.com>
2017-07-18 14:51:24 -0400

    creating total price reduce function and changed tab title

--- scripts/more-functions.js
+++ scripts/more-functions.js
@@ -9,3 +9,14 @@ function product(array) {
 }
 
 console.log(product([1,2,3,4]));
+
+// TOTAL PRICE
+
+var products = [
+    { name: 'Basketball', price: 12.00 },
+    { name: 'Tennis Racquet', price: 66.00 },
+    { name: 'Tennis Balls', price: 9.00 },
+    { name: 'Tennis Balls', price: 9.00 }
+];
+
+products.reduce( function(acc))
